4b2b0e291a6136d3f95d9bc26641ac11d6681e69
Mario Prieta Sánchez <145470491+mariops03@users.noreply.github.com>
2024-01-31 01:15:04 +0100

    Refactor AddView.vue component

--- web/src/views/AddView.vue
+++ web/src/views/AddView.vue
@@ -6,41 +6,89 @@
         label="Nombre del ejercicio"
         required
       ></v-text-field>
-      <v-text-field
+
+      <v-select
         v-model="exercise.force"
+        :items="['pull', 'push']"
         label="Tipo de fuerza"
         required
-      ></v-text-field>
-      <v-text-field
+      ></v-select>
+
+      <v-select
         v-model="exercise.level"
+        :items="['beginner', 'intermediate', 'advanced']"
         label="Nivel"
         required
-      ></v-text-field>
-      <v-text-field v-model="exercise.mechanic" label="Mecánica"></v-text-field>
-      <v-text-field
+      ></v-select>
+
+      <v-select
+        v-model="exercise.mechanic"
+        :items="['compound', 'isolation']"
+        label="Mecánica"
+      ></v-select>
+
+      <v-select
         v-model="exercise.equipment"
+        :items="[
+          'dumbbell',
+          'barbell',
+          'cable',
+          'machine',
+          'body only',
+          'other',
+        ]"
         label="Equipamiento"
         required
-      ></v-text-field>
-      <v-text-field
+      ></v-select>
+
+      <v-select
         v-model="exercise.primaryMuscles"
+        :items="MusclesOptions"
         label="Músculos principales"
+        multiple
         required
-      ></v-text-field>
-      <v-text-field
+        filterable
+      ></v-select>
+
+      <v-select
         v-model="exercise.secondaryMuscles"
+        :items="MusclesOptions"
         label="Músculos secundarios (Opcional)"
-      ></v-text-field>
-      <v-textarea
-        v-model="exercise.instructions"
-        label="Instrucciones"
-        required
-      ></v-textarea>
-      <v-text-field
+        multiple
+        filterable
+      ></v-select>
+
+      <v-row v-for="(instruction, index) in exercise.instructions" :key="index">
+        <v-col cols="10">
+          <v-textarea
+            v-model="exercise.instructions[index]"
+            :label="'Instrucción ' + (index + 1)"
+            required
+          ></v-textarea>
+        </v-col>
+        <v-col cols="2">
+          <v-btn text @click="removeInstruction(index)">
+          <v-icon color="red darken-2">mdi-delete</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-btn class="mb-5" @click="addInstruction">Añadir Instrucción</v-btn>
+
+      <v-select
         v-model="exercise.category"
+        :items="[
+          'strength',
+          'powerlifting',
+          'stretching',
+          'plyometrics',
+          'olympic weightlifting',
+          'strongman',
+        ]"
         label="Categoría"
         required
-      ></v-text-field>
+      ></v-select>
+
       <v-text-field v-model="exercise.images" label="Imágenes"></v-text-field>
       <v-btn type="submit" color="primary">Guardar</v-btn>
     </v-form>
@@ -59,12 +107,30 @@ export default {
         level: "",
         mechanic: "",
         equipment: "",
-        primaryMuscles: "",
-        secondaryMuscles: "",
-        instructions: "",
+        primaryMuscles: [],
+        secondaryMuscles: [],
+        instructions: [],
         category: "",
         images: "",
       },
+      MusclesOptions: [
+        "Biceps",
+        "Triceps",
+        "Deltoids",
+        "Pectorals",
+        "Abs",
+        "Obliques",
+        "Quads",
+        "Hamstrings",
+        "Calves",
+        "Glutes",
+        "Lats",
+        "Traps",
+        "Forearms",
+        "Hip Flexors",
+        "Adductors",
+        "Abductors",
+      ],
     };
   },
   methods: {
@@ -78,13 +144,11 @@ export default {
           equipment,
           primaryMuscles,
           secondaryMuscles,
-          instructions,
           category,
           images,
         } = this.exercise;
 
         const id = name.replace(/ /g, "_");
-
         const response = await axios.post(
           "http://localhost:3001/api/v1/createExcersise",
           {
@@ -95,7 +159,7 @@ export default {
             equipment,
             primaryMuscles,
             secondaryMuscles,
-            instructions,
+            instructions: this.exercise.instructions,
             category,
             images,
             id,
@@ -128,13 +192,19 @@ export default {
         level: "",
         mechanic: "",
         equipment: "",
-        primaryMuscles: "",
-        secondaryMuscles: "",
-        instructions: "",
+        primaryMuscles: [],
+        secondaryMuscles: [],
+        instructions: [],
         category: "",
         images: "",
       };
     },
+    addInstruction() {
+      this.exercise.instructions.push("");
+    },
+    removeInstruction(index) {
+    this.exercise.instructions.splice(index, 1);
+  },
   },
 };
 </script>
