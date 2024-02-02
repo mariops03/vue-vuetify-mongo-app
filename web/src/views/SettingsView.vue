f7af7952273c7e0030fe55f4e32fc5a6c3ceaa74
Mario Prieta Sánchez <145470491+mariops03@users.noreply.github.com>
2024-02-02 01:08:15 +0100

    Refactor SettingsView.vue component

--- web/src/views/SettingsView.vue
+++ web/src/views/SettingsView.vue
@@ -1,18 +1,37 @@
 <template>
-    <div>
-      <p>HOLAAAAAAA</p>
-    </div>
-  </template>
-  
-  <script>
-    
-  export default {
-    data() {
-      return {
-        user: null,
-        userMessage: "",
-      };
-    },
+  <v-main>
+    <v-row justify="center" class="mt-3">
+      <v-col cols="6" >
+        <v-container>
+          <h1>Informacion del usuario</h1>
+          <v-card class="mt-8">
+            <v-row justify="center" align="center">
+              <v-col cols="6">
+                <h3>Nombre del usuario</h3>
+                <h3>Correo electronico del usuario</h3>
+              </v-col>
+              <v-col cols="6">
+                <h2 hover>{{ user.username }}</h2>
+                <h2>{{ user.email }}</h2>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-main>
+</template>
 
-  };
-  </script>
+<script>
+import { useUserStore } from "../store/user";
+
+export default {
+  setup() {
+    const userStore = useUserStore();
+
+    return {
+      user: userStore.getCurrentUser,
+    };
+  },
+};
+</script>
